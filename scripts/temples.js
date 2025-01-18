a18a30f47ad078359346dcbde073f78d0c352bf5
thebestknown <jeimyespinoza971@gmail.com>
2025-01-17 21:57:04 -0500

    arranged css

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,8 +1,10 @@
-const hamburger = document.querySelector(".hamburger");
-const nav = document.querySelector("nav");
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
 
-hamburger.addEventListener("click", () => {
-    nav.style.display = nav.style.display === "block" ? "none" : "block";
+hamburger.addEventListener('click', () => {
+    const isOpen = hamburger.classList.toggle('open'); // Alterna la clase "open"
+    nav.style.display = isOpen ? 'block' : 'none'; // Muestra u oculta el menú
 });
+
 
 document.getElementById("lastModified").textContent = document.lastModified;
