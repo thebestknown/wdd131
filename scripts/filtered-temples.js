40b1aee68b89527f1c8f4d20f98136203832460b
thebestknown <jeimyespinoza971@gmail.com>
2025-01-30 16:23:27 -0500

    Finished 3 first steps Html

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,9 @@
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+});
+
+document.getElementById("lastModified").textContent = document.lastModified;
