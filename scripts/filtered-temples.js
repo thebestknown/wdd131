f83abac45c311797692a588c9c534bea03048651
thebestknown <jeimyespinoza971@gmail.com>
2025-01-31 06:44:59 -0500

    Changes in the css

--- scripts/filtered-temples.js
+++ scripts/filtered-temples.js
@@ -137,3 +137,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
     document.getElementById("lastModified").textContent = document.lastModified;
 });
+
+const hamburger = document.querySelector('.hamburger');
+const nav = document.querySelector('nav');
+
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('open');
+    nav.style.display = nav.style.display === 'block' ? 'none' : 'block';
+});
